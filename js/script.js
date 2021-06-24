32020a7254056a839edd4f2aab3f6c63ac957163
yus-torres <info@synthesisgroup.co>
2021-06-24 11:41:38 +0800

    Updates

--- js/script.js
+++ js/script.js
@@ -447,18 +447,23 @@
 
 })(window.jQuery);
 
+// Bootstrap Carousel
+$('.carousel').carousel({
+	interval: 5000
+})
+
 //
-$(function() {
-	$('a[href*="#"]:not([href="#"])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: target.offset().top
-				}, 1000);
-				return false;
-			}
-		}
-	});
-});
+//$(function() {
+//	$('a[href*="#"]:not([href="#"])').click(function() {
+//		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//			var target = $(this.hash);
+//			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//			if (target.length) {
+//				$('html, body').animate({
+//					scrollTop: target.offset().top
+//				}, 1000);
+//				return false;
+//			}
+//		}
+//	});
+//});
